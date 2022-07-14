65097e5803fc80b66ac74b883886e442dd1cb87f
Aamer A <ar.aamer@gmail.com>
2022-07-14 15:55:01 -0400

    single loss

--- CUB/train_custom.py
+++ CUB/train_custom.py
@@ -130,95 +130,6 @@ def run_epoch(model, optimizer, loader, loss_meter, acc_meter, criterion, attr_c
     return loss_meter, acc_meter
 
 
-def run_epoch_custom(model, optimizer, loader, loss_meter, acc_meter, criterion, attr_criterion, args, is_training):
-    """
-    For the rest of the networks (X -> A, cotraining, simple finetune)
-    """
-    if is_training:
-        model.train()
-    else:
-        model.eval()
-
-    for _, data in enumerate(loader):
-        if attr_criterion is None:
-            inputs, labels = data
-            attr_labels, attr_labels_var = None, None
-        else:
-            inputs, labels, attr_labels = data
-            
-            if args.n_attributes > 1:
-                attr_labels = [i.long() for i in attr_labels]
-                attr_labels = torch.stack(attr_labels).t()#.float() #N x 312
-            else:
-                if isinstance(attr_labels, list):
-                    attr_labels = attr_labels[0]
-                attr_labels = attr_labels.unsqueeze(1)
-            attr_labels_var = torch.autograd.Variable(attr_labels).float()
-            attr_labels_var = attr_labels_var.cuda() if torch.cuda.is_available() else attr_labels_var
-        
-
-        inputs_var = torch.autograd.Variable(inputs)
-        inputs_var = inputs_var.cuda() if torch.cuda.is_available() else inputs_var
-        labels_var = torch.autograd.Variable(labels)
-        labels_var = labels_var.cuda() if torch.cuda.is_available() else labels_var
-    
-        if is_training and args.use_aux:
-            outputs, aux_outputs = model(inputs_var)
-            losses = []
-            out_start = 0
-            print(attr_labels_var)
-            #print(labels_var)
-            #print("outputs shape: " + str(outputs.shape) + "  | attr_labels shape: " + str(attr_labels_var.shape))
-
-            if not args.bottleneck: #loss main is for the main task label (always the first output)
-                loss_main = 1.0 * criterion(outputs[0], labels_var) + 0.4 * criterion(aux_outputs[0], labels_var)
-                losses.append(loss_main)
-                out_start = 1
-            if attr_criterion is not None and args.attr_loss_weight > 0: #X -> A, cotraining, end2end
-                #for i in range(len(attr_criterion)):
-                #    losses.append(args.attr_loss_weight * (1.0 * attr_criterion[i](outputs[i+out_start].squeeze().type(torch.cuda.FloatTensor), attr_labels_var[:, i]) \
-                #                                            + 0.4 * attr_criterion[i](aux_outputs[i+out_start].squeeze().type(torch.cuda.FloatTensor), attr_labels_var[:, i])))
-                #losses = args.attr_loss_weight * (1.0 * attr_criterion(outputs.type(torch.cuda.FloatTensor), attr_labels_var \
-                #                                            + 0.4 * attr_criterion(aux_outputs[i+out_start].squeeze().type(torch.cuda.FloatTensor), attr_labels_var[:, i])))
-                losses = args.attr_loss_weight * criterion(torch.tensor(outputs).cuda(), attr_labels_var) + 0.4 * criterion(torch.tensor(aux_outputs).cuda(), attr_labels_var)
-
-        else: #testing or no aux logits
-            outputs = model(inputs_var)
-            losses = []
-            out_start = 0
-            if not args.bottleneck:
-                loss_main = criterion(outputs[0], labels_var)
-                losses.append(loss_main)
-                out_start = 1
-            if attr_criterion is not None and args.attr_loss_weight > 0: #X -> A, cotraining, end2end
-                #for i in range(len(attr_criterion)):
-                #    losses.append(args.attr_loss_weight * attr_criterion[i](outputs[i+out_start].squeeze().type(torch.cuda.FloatTensor), attr_labels_var[:, i]))
-                losses = args.attr_loss_weight * criterion(torch.tensor(outputs).cuda(), attr_labels_var) + 0.4 * criterion(torch.tensor(aux_outputs).cuda(), attr_labels_var)
-
-
-        if args.bottleneck: #attribute accuracy
-            sigmoid_outputs = torch.nn.Sigmoid()(torch.cat(outputs, dim=1))
-            acc = binary_accuracy(sigmoid_outputs, attr_labels)
-            acc_meter.update(acc.data.cpu().numpy(), inputs.size(0))
-        else:
-            acc = accuracy(outputs[0], labels, topk=(1,)) #only care about class prediction accuracy
-            acc_meter.update(acc[0], inputs.size(0))
-
-        if attr_criterion is not None:
-            if args.bottleneck:
-                total_loss = sum(losses)/ args.n_attributes
-            else: #cotraining, loss by class prediction and loss by attribute prediction have the same weight
-                total_loss = losses[0] + sum(losses[1:])
-                if args.normalize_loss:
-                    total_loss = total_loss / (1 + args.attr_loss_weight * args.n_attributes)
-        else: #finetune
-            total_loss = losses
-        loss_meter.update(total_loss.item(), inputs.size(0))
-        if is_training:
-            optimizer.zero_grad()
-            total_loss.backward()
-            optimizer.step()
-    return loss_meter, acc_meter
 
 
 def train(model, args):
@@ -292,8 +203,7 @@ def train(model, args):
         if args.no_img:
             train_loss_meter, train_acc_meter = run_epoch_simple(model, optimizer, train_loader, train_loss_meter, train_acc_meter, criterion, args, is_training=True)
         else:
-            #train_loss_meter, train_acc_meter = run_epoch(model, optimizer, train_loader, train_loss_meter, train_acc_meter, criterion, attr_criterion, args, is_training=True)
-            train_loss_meter, train_acc_meter = run_epoch_custom(model, optimizer, train_loader, train_loss_meter, train_acc_meter, criterion, attr_criterion, args, is_training=True)
+            train_loss_meter, train_acc_meter = run_epoch(model, optimizer, train_loader, train_loss_meter, train_acc_meter, criterion, attr_criterion, args, is_training=True)
  
         if not args.ckpt: # evaluate on val set
             val_loss_meter = AverageMeter()
