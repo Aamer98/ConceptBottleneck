a24cc2b66b28dad7e07d4e75cf4420ba6d61d79e
Aamer A <ar.aamer@gmail.com>
2022-07-14 22:28:58 -0400

    cuda

--- CUB/train_custom.py
+++ CUB/train_custom.py
@@ -54,6 +54,7 @@ def run_epoch(model, optimizer, loader, loss_meter, acc_meter, criterion, attr_c
     """
     For the rest of the networks (X -> A, cotraining, simple finetune)
     """
+    print(model)
     if is_training:
         model.train()
     else:
